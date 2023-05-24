83331da079001e8a78908d6a7d96a5559bde2032
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-05-24 13:40:42 -0600

    feat(): validating data with class-validator & mapped-types

--- src/dtos/products.dto.ts
+++ src/dtos/products.dto.ts
@@ -1,10 +1,33 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUrl,
+} from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 
 export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString({ message: 'We need a valid name' })
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
   readonly price: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
   readonly stock: number;
+
+  @IsNotEmpty()
+  @IsUrl({ require_protocol: true })
   readonly image: string;
 }
 
