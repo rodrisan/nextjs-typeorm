f393f7ad1a207e30a1b5a3f628fb5fc403c39543
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-05-24 17:52:06 -0600

    feat(): export services & import modules

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,9 +4,10 @@ import { CustomersController } from './controllers/customers.controller';
 import { CustomersService } from './services/customers.service';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
+import { ProductsModule } from '../products/products.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule],
   controllers: [CustomersController, UsersController],
   providers: [CustomersService, UsersService],
 })
