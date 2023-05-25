7038fad8a63c0154e6021a48f668277a210b255c
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-05-24 18:06:38 -0600

    feat(): working with useValue as dependency injection

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -5,9 +5,18 @@ import { UsersModule } from '../users/users.module';
 import { ProductsModule } from '../products/products.module';
 import { OrdersModule } from '../orders/orders.module';
 
+const API_KEY = '123456789';
+const API_KEY_PROD = 'PROD123456789';
+
 @Module({
   imports: [UsersModule, ProductsModule, OrdersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+  ],
 })
 export class AppModule {}
