d1dcf09583bb4badd44ea6a8f5567174a18f94f8
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-05-25 10:07:53 -0600

    feat(): working with global modules

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -8,19 +8,19 @@ import { ProductsModule } from '../products/products.module';
 import { OrdersModule } from '../orders/orders.module';
 import { todosApi } from 'src/common/constants';
 import { lastValueFrom } from 'rxjs';
-
-const API_KEY = '123456789';
-const API_KEY_PROD = 'PROD123456789';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, OrdersModule, HttpModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    OrdersModule,
+    HttpModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
     {
       provide: 'todosApi',
       useValue: todosApi,
