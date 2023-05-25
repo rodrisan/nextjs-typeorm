6fc52f412cdd238ad391658edd1c36e4d7afbfa2
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-05-25 11:10:04 -0600

    feat(): working with variables comming from .env files

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,18 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
   constructor(
-    @Inject('API_KEY') private apiKey: string,
+    // @Inject('API_KEY') private apiKey: string,
+    private _configService: ConfigService,
     @Inject('TASKS') private tasks: any[],
   ) {}
 
   getHello(): string {
+    const apiKey = this._configService.get<string>('API_KEY');
+    const dbName = this._configService.get<string>('DATABASE_NAME');
     console.log(this.tasks);
-    return `Hello World! ${this.apiKey}`;
+    return `Hello World! ${apiKey} ${dbName}`;
   }
 }
---
+++ src/common/environments.ts
@@ -0,0 +1,6 @@
+export const environments = {
+  dev: '.env.dev',
+  prod: '.env.prod',
+  stage: '.env.stage',
+  local: '.env.local',
+};
