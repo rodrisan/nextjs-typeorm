6843c9674375388be8dafb3ec34bf3a2e491cea6
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-05-25 16:15:26 -0600

    feat(): using Swagger decorators

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -8,34 +8,41 @@ import {
   Delete,
   ParseIntPipe,
 } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { UsersService } from '../services/users.service';
 import { CreateUserDto, UpdateUserDto } from '../dtos/user.dto';
 
+ApiTags('Users');
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Get all Users' })
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get a User by ID' })
   @Get(':id')
   get(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Get all the Orders by a User' })
   @Get(':id/orders')
   getOrders(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.getOrdersByUser(id);
   }
 
+  @ApiOperation({ summary: 'Create a new User' })
   @Post()
   create(@Body() payload: CreateUserDto) {
     return this.usersService.create(payload);
   }
 
+  @ApiOperation({ summary: 'Update an existing Customer' })
   @Put(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -44,6 +51,7 @@ export class UsersController {
     return this.usersService.update(id, payload);
   }
 
+  @ApiOperation({ summary: 'Delete an existing Customer' })
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.remove(+id);
