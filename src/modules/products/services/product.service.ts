f909e9a15d378223b10c1e22ad898f54a4a7fd8c
Rodrigo Sarmiento <rodrigo.sarmiento@yaydoo.com>
2023-05-26 18:49:32 -0600

    feat(): use repository pattern to query the db

--- src/modules/products/services/product.service.ts
+++ src/modules/products/services/product.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 import { Product } from 'src/modules/products/entities/product.entity';
 import {
@@ -8,70 +10,62 @@ import {
 
 @Injectable()
 export class ProductService {
-  private counter = 0;
-  private products: Product[] = [
-    {
-      description: 'Description Product 1',
-      id: 1,
-      image: '',
-      name: 'Product 1',
-      price: 2323,
-      stock: 123,
-    },
-  ];
+  constructor(
+    @InjectRepository(Product) private _productRepository: Repository<Product>,
+  ) {}
 
   findAll() {
-    return this.products;
+    return this._productRepository.find();
   }
 
   findOne(id: number) {
-    const product = this.products.find((item) => item.id === id);
+    const product = this._productRepository.findOneBy({ id });
     if (!product) {
       throw new NotFoundException(`Product #${id} not found`);
     }
     return product;
   }
 
-  create(payload: CreateProductDto) {
-    this.counter += 1;
-    const newProduct = {
-      id: this.counter,
-      ...payload,
-    };
-    this.products.push(newProduct);
-    return newProduct;
-  }
+  // create(payload: CreateProductDto) {
+  //   this.counter += 1;
+  //   const newProduct = {
+  //     id: this.counter,
+  //     ...payload,
+  //   };
+  //   this.products.push(newProduct);
+  //   return newProduct;
+  // }
 
-  update(id: number, payload: UpdateProductDto) {
-    // const productIndex = this.products.findIndex((item) => item.id === id);
-    // if (productIndex === -1) throw new Error('Not found');
-    // this.products[productIndex] = {
-    //   id: id,
-    //   ...payload,
-    // };
-    // return {
-    //   data: this.products[productIndex],
-    //   message: 'Product updated succesfully',
-    // };
+  // update(id: number, payload: UpdateProductDto) {
+  //   // const productIndex = this.products.findIndex((item) => item.id === id);
+  //   // if (productIndex === -1) throw new Error('Not found');
+  //   // this.products[productIndex] = {
+  //   //   id: id,
+  //   //   ...payload,
+  //   // };
+  //   // return {
+  //   //   data: this.products[productIndex],
+  //   //   message: 'Product updated succesfully',
+  //   // };
 
-    const product = this.findOne(id);
-    if (product) {
-      const index = this.products.findIndex((item) => item.id === id);
-      this.products[index] = {
-        ...product,
-        ...payload,
-      };
-      return this.products[index];
-    }
-    return null;
-  }
+  //   const product = this.findOne(id);
+  //   if (product) {
+  //     const index = this.products.findIndex((item) => item.id === id);
+  //     this.products[index] = {
+  //       ...product,
+  //       ...payload,
+  //     };
+  //     return this.products[index];
+  //   }
+  //   return null;
+  // }
 
-  remove(id: number) {
-    const index = this.products.findIndex((item) => item.id === id);
-    if (index === -1) {
-      throw new NotFoundException(`Product #${id} not found`);
-    }
-    this.products.splice(index, 1);
-    return true;
-  }
+  // remove(id: number) {
+  //   const index = this.products.findIndex((item) => item.id === id);
+  //   if (index === -1) {
+  //     throw new NotFoundException(`Product #${id} not found`);
+  //   }
+  //   this.products.splice(index, 1);
+  //   return true;
+  // }
 }
