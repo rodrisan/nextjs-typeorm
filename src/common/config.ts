7f32cf13f5bd20205399fa3ef6a8baf27ee62a16
Rodrigo Sarmiento <rodrigo.sarmiento@yaydoo.com>
2023-05-27 11:06:25 -0600

    feat(): separate variables for two database connections

--- src/common/config.ts
+++ src/common/config.ts
@@ -2,12 +2,19 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('config', () => {
   return {
-    database: {
-      host: process.env.DATABASE_HOST,
-      name: process.env.DATABASE_NAME,
-      port: parseInt(process.env.DATABASE_PORT),
-      user: process.env.DATABASE_USER,
-      pass: process.env.DATABASE_PASS,
+    mysql: {
+      dbHost: process.env.MYSQL_HOST,
+      dbName: process.env.MYSQL_DB_NAME,
+      dbPort: parseInt(process.env.MYSQL_PORT),
+      dbUser: process.env.MYSQL_USER,
+      dbPass: process.env.MYSQL_PASS,
+    },
+    postgres: {
+      dbHost: process.env.POSTGRES_HOST,
+      dbName: process.env.POSTGRES_NAME,
+      dbPort: parseInt(process.env.POSTGRES_PORT),
+      dbUser: process.env.POSTGRES_USER,
+      dbPass: process.env.POSTGRES_PASS,
     },
     allowCors: process.env.ALLOW_CORS,
     apiKey: process.env.API_KEY,
