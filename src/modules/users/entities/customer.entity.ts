9f1a444ecb3baa9b52ad8170b84dfdb5252610f0
Rodrigo Sarmiento <rodrigo.sarmiento@yaydoo.com>
2023-05-26 18:35:46 -0600

    feat(): creating entities using typeorm

--- src/modules/users/entities/customer.entity.ts
+++ src/modules/users/entities/customer.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'customers' })
 export class Customer {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar' })
   name: string;
+
+  @Column({ type: 'varchar' })
   lastName: string;
+
+  @Column({ type: 'varchar' })
   phone: string;
 }
