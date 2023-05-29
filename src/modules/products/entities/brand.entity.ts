06341e4b221ba9407f4ff70ed87ae7c8de2f42f8
Rodrigo Sarmiento <rodrigo.sarmiento@yaydoo.com>
2023-05-29 11:23:49 -0600

    feat(): create 1-N relations in products & brands

--- src/modules/products/entities/brand.entity.ts
+++ src/modules/products/entities/brand.entity.ts
@@ -1,12 +1,16 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 
 import { RootEntity } from '../../../common/root-entity';
+import { Product } from './product.entity';
 
 @Entity({ name: 'brands' })
 export class Brand extends RootEntity {
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', unique: true })
   name: string;
 
   @Column({ type: 'varchar' })
   image: string;
+
+  @OneToMany(() => Product, (product) => product.brand)
+  products: Product[];
 }
