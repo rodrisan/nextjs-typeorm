eec6ddefa287bbbdde5825726c74ea2469c5c458
Rodrigo Sarmiento <rodrigo.sarmiento@yaydoo.com>
2023-05-29 09:41:27 -0600

    feat(): update root-entity

--- src/common/root-entity.ts
+++ src/common/root-entity.ts
@@ -4,18 +4,20 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
-export class RootEntity {
-  @PrimaryGeneratedColumn()
+export abstract class RootEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: number;
 
   @CreateDateColumn({
     type: 'timestamptz',
+    name: 'created_at',
     default: () => 'CURRENT_TIMESTAMP',
   })
   createAt: Date;
 
   @UpdateDateColumn({
     type: 'timestamptz',
+    name: 'updated_at',
     default: () => 'CURRENT_TIMESTAMP',
   })
   updateAt: Date;
