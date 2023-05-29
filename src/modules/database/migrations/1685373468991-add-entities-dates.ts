ffde267fc39637a7aadc39295c3b93ca0b5bea3f
Rodrigo Sarmiento <rodrigo.sarmiento@yaydoo.com>
2023-05-29 09:50:51 -0600

    chore(): format migrations!

--- src/modules/database/migrations/1685373468991-add-entities-dates.ts
+++ src/modules/database/migrations/1685373468991-add-entities-dates.ts
@@ -1,24 +1,35 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddEntitiesDates1685373468991 implements MigrationInterface {
-    name = 'AddEntitiesDates1685373468991'
+  name = 'AddEntitiesDates1685373468991';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "brands" ADD "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`);
-        await queryRunner.query(`ALTER TABLE "brands" ADD "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`);
-        await queryRunner.query(`ALTER TABLE "customers" ADD "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`);
-        await queryRunner.query(`ALTER TABLE "customers" ADD "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`);
-        await queryRunner.query(`ALTER TABLE "users" ADD "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`);
-        await queryRunner.query(`ALTER TABLE "users" ADD "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "brands" ADD "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "brands" ADD "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "customers" ADD "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "customers" ADD "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "updatedAt"`);
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "createdAt"`);
-        await queryRunner.query(`ALTER TABLE "customers" DROP COLUMN "updatedAt"`);
-        await queryRunner.query(`ALTER TABLE "customers" DROP COLUMN "createdAt"`);
-        await queryRunner.query(`ALTER TABLE "brands" DROP COLUMN "updatedAt"`);
-        await queryRunner.query(`ALTER TABLE "brands" DROP COLUMN "createdAt"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "updatedAt"`);
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "createdAt"`);
+    await queryRunner.query(`ALTER TABLE "customers" DROP COLUMN "updatedAt"`);
+    await queryRunner.query(`ALTER TABLE "customers" DROP COLUMN "createdAt"`);
+    await queryRunner.query(`ALTER TABLE "brands" DROP COLUMN "updatedAt"`);
+    await queryRunner.query(`ALTER TABLE "brands" DROP COLUMN "createdAt"`);
+  }
 }
